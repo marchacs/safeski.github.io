fa7b1cd1f57b4fee2bd08c44d99fb085a06279df
Christy Marchand <59838977+marchacs@users.noreply.github.com>
2022-02-21 11:15:21 -0500

    merged files– now need to set up pwa for all files

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+       e.waitUntil(
+           caches.open("static").then(cache=> {
+                return cache.addAll(["./", "./src/master.css", "./images/logo192.png" ]) 
+
+           })
+       );
+
+});
+
+self.addEventListener("fetch", e => { 
+    console.log('Interceptin fetch request for: $(e.request_url)');
+});
