efea9600202f6741a959621b32582e1551a7ca18
Christy Marchand <59838977+marchacs@users.noreply.github.com>
2022-03-03 08:50:09 -0500

    MAps doing what I want them to do(:

--- script.js
+++ script.js
@@ -3,24 +3,23 @@
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
 let map, infoWindow;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat:39.4808, lng:-106.0676 },
-    zoom: 15,
-    heading: 100,
-    tilt: 47.5,
-
+  const myLatLng = { lat:39.4808, lng:-106.0676 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: myLatLng,
   });
+
+ 
   infoWindow = new google.maps.InfoWindow();
 
   const locationButton = document.createElement("button");
 
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
+document.getElementById('findMe').addEventListener("click", () => {
+  // locationButton.textContent = "Pan to Current Location";
+  // locationButton.classList.add("custom-map-control-button");
+  // map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  // locationButton.addEventListener("click", () => {
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -29,10 +28,14 @@ function initMap() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
+          new google.maps.Marker({
+            position: pos,
+            map,
+          });
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
+          // infoWindow.setPosition(pos);
+          // infoWindow.setContent("Location found.");
+          // infoWindow.open(map);
           map.setCenter(pos);
         },
         () => {
@@ -44,6 +47,11 @@ function initMap() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
+
+
+
+
+  
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -55,3 +63,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
   infoWindow.open(map);
 }
+
+
+
+
