07f5064f63d9f42e02b015c211ffcb0942a040c2
Christy Marchand <59838977+marchacs@users.noreply.github.com>
2022-03-22 09:48:59 -0400

    i am angry

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-// global var 
-var pos;
-
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBcrulFBbF6HnawvRDlGFOhqTJJfRtbGTM",
-authDomain: "safeski-7c162.firebaseapp.com",
-databaseURL: "https://safeski-7c162-default-rtdb.firebaseio.com",
-projectId: "safeski-7c162",
-storageBucket: "safeski-7c162.appspot.com",
-messagingSenderId: "95077250865",
-appId: "1:95077250865:web:2c0cf9e05bde5716fca210"
-};
-
-const firestore = getFirestore();
-
-
-
 let map, infoWindow;
 function initMap() {
   const myLatLng = { lat:39.4808, lng:-106.0676 };
@@ -29,43 +6,32 @@ function initMap() {
    
     center: myLatLng,
   });
-
  
   infoWindow = new google.maps.InfoWindow();
-
   // const locationButton = document.createElement("button");
-
-
-  // function addUserTrail(){
-  //   const userTrailName = document.querySelector('#userTrailName');
-  //   userTrailName.innerHTML = pos;
-  //   console.log(pos);
-  // }
-    
-
 document.getElementById('findMe').addEventListener("click", () => {
-  
+  // locationButton.textContent = "Pan to Current Location";
+  // locationButton.classList.add("custom-map-control-button");
+  // map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  // locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-           pos = {
+          const pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
-         addUserTrail();
-
-
           new google.maps.Marker({
-            position: pos,
-            map,
-          });
-          alert('Ski Patrol is on the way!(Not actually this is a test)');
-          // infoWindow.setPosition(pos);
-          // infoWindow.setContent("Location found.");
-          // infoWindow.open(map);
+             position: pos,
+             map,
+           });
+           
+           alert('Ski Patrol is on the way!(Not actually this is a test)');
+           // infoWindow.setPosition(pos);
+           // infoWindow.setContent("Location found.");
+           // infoWindow.open(map);
           map.setCenter(pos);
-         
-
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
@@ -76,16 +42,8 @@ document.getElementById('findMe').addEventListener("click", () => {
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
-
+  
 }
-//dashboard button
-// document.getElementById('dashboardBtn').addEventListener("click", () => {
-//   window.location.href = 'skipatroldashboard.html'
-// });
-
-
-
-
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(
@@ -95,4 +53,3 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
   infoWindow.open(map);
 }
-
