f0aaeb95e857e07ad1e0f6f5014dd03412e9f13e
Christy Marchand <59838977+marchacs@users.noreply.github.com>
2022-04-10 17:40:02 -0400

    cleaning up!!!!

--- script.js
+++ script.js
@@ -116,3 +116,11 @@ document.getElementById('locationSent').addEventListener("click", () => {
     ]
   }
 ]
+
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
